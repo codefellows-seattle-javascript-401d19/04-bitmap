79f043cc8808d986ab1dbf87ab70d69b86b40109
Catherine Looper <loopercat@gmail.com>
2017-11-30 15:47:18 -0800

    Scaffolded bitmap test

--- lab-catherine-kerry/__test__/bitmap-test.js
+++ lab-catherine-kerry/__test__/bitmap-test.js
@@ -1,5 +1,18 @@
 'use strict';
 
 const bitmap = require('../lib/bitmap');
+const fs = require('fs');
 
-console.log(bitmap);
+describe('lib/bitmap.js', () => {
+
+  describe('testing for valid bitmap file paths', () => {
+
+    test('testing to see that we are receiving expected file paths ', () => {
+      fs.readFile(`${__dirname}/asset/house.bmp`, (error, data) => {
+        let parsedBitMap = bitmap.parseBitmap(data);
+        expect(error).toBeNull();
+        expect(parsedBitMap).not.toBeNull();
+      });
+    });
+  });
+});
