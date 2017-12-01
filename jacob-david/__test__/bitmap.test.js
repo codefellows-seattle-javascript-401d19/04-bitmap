2e712d3f50f8a71d69045799b975384b863b1d51
David Lindahl <davidalindahl@gmail.com>
2017-12-01 15:05:02 -0800

    added basic transform and bitmap tests

--- jacob-david/__test__/bitmap.test.js
+++ jacob-david/__test__/bitmap.test.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const bitmap = require('../bitmap');
+
+describe('bitmap-test.js', () => {
+  test('the test to make sure our bitmap rendered. i think', (done => {
+
+    bitmap.readFile((error,data) => {
+      expect(error).toBeNull();
+      expect(data).toEqual('new bitmap question mark');
+      done();
+    });
+  }));
+});
