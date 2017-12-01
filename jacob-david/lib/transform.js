a8411d44113d9450c742fcf44387fad09e853e8f
David Lindahl <davidalindahl@gmail.com>
2017-12-01 12:05:03 -0800

    added in some notes from class tips

--- jacob-david/lib/transform.js
+++ jacob-david/lib/transform.js
@@ -3,11 +3,13 @@
 const transform = module.exports = {};
 const parsedData = require('./lib/bitmap');
 
+// TODO: pass in the callback next to the buffer
 transform.invertColors = (buffer) => {
   let transformedBitmap = {};
 
-  
+  // TODO: write a for loop using the length of the color table, use write to make changes, incrememnt in factor of 4  (4  bytes at a time)
     
+  // this is just test code, not sure if it works
   transformedBitmap.color = buffer.fill(97);
   
 
