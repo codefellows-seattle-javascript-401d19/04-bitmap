400f1b151369dc30ce3bb7ea4cf01fd6059c2639
Predrag Josifovic <pjosifovic@yahoo.com>
2017-11-30 17:34:44 -0800

    cleaning up read-img.js

--- lab-pedja-jeff-cameron/lib/read-img.js
+++ lab-pedja-jeff-cameron/lib/read-img.js
@@ -8,7 +8,6 @@ readImg.getFile = (inputFilePath) => {
   const HEIGHT_OFFSET = 22;
   const PIXEL_TABLE_OFFSET = 10;
   const FILE_SIZE_OFFSET = 2;
-  const HEADER_HEIGHT = 0;
   const DIB_HEADER = 14;
 
   fs.readFile(inputFilePath, (error, data) => {
@@ -16,19 +15,15 @@ readImg.getFile = (inputFilePath) => {
       console.error(error);
       return;
     }
-    // let parsedBitmap = bitmap.parseBitmap(data);
-    // console.log(parsedBitmap);
 
     function ConstructBitmap(buffer) {
       this.buffer = buffer;
       this.fileSize = buffer.readInt32LE(FILE_SIZE_OFFSET);
       this.type = buffer.toString('utf-8',0,2);
-      this.headerHeight = buffer.readInt32LE(HEADER_HEIGHT);
       this.pixelTableOffset = buffer.readInt32LE(PIXEL_TABLE_OFFSET);
       this.height = buffer.readInt32LE(HEIGHT_OFFSET);
       this.dibHeader = buffer.readInt32LE(DIB_HEADER);
-      this.colorTable = buffer.slice(this.headerHeight+this.dibHeader, this.pixelTableOffset-1);
-      // this.colorTable = buffer.toString('utf-8', 257, 821)
+      this.colorTable = buffer.slice(54, this.pixelTableOffset-1);
     }
 
     var constructedBitmap = new ConstructBitmap(data);
