9b84cd2c3cf160fee1b3353adbfac27645f33a6c
David Lindahl <davidalindahl@gmail.com>
2017-12-01 15:44:25 -0800

    added bit per pixel offset and width offset to variables on bitmap file and added the parsed bitmap lines accordingly

--- jacob-david/__test__/transform.test.js
+++ jacob-david/__test__/transform.test.js
@@ -3,12 +3,12 @@
 const transform = require('../lib/transform');
 
 describe('transform-test.js', () => {
-    test('the transformation we applied should be returned if there are no errors', (done) {
+  test('the transformation we applied should be returned if there are no errors', (done) => {
 
     transform.readFile((error,data) => {
-        expect(error).toBeNull();
-        expect(data).toEqual('whatever we want after transformation');
-        done();
+      expect(error).toBeNull();
+      expect(data).toEqual('whatever we want after transformation');
+      done();
     });
-    });
+  });
 });
--- jacob-david/lib/bitmap.js
+++ jacob-david/lib/bitmap.js
@@ -10,15 +10,19 @@ bitmap.parseBitmap = (buffer) => {
   const FILE_SIZE_OFFSET = 2;
   const HEADER_OFFSET = 22;
   const COLOR_OFFSET = 18;
+  const BIT_PER_PIXEL_OFFSET = 28;
+  const WIDTH_OFFSET = 18;
 
   parsedBitmap.buffer = buffer;
 
   parsedBitmap.type = buffer.toString('utf-8',0,2);
-  parsedBitmap.fileSize = buffer.readInt32LE(FILE_SIZE_OFFSET);
-  parsedBitmap.pixelTableOffset = buffer.readInt32LE(PIXEL_TABLE_OFFSET);
-  parsedBitmap.height = buffer.readInt32LE(HEIGHT_OFFSET);
-  parsedBitmap.headerSize = buffer.readInt32LE(HEADER_OFFSET);
-  parsedBitmap.colorOffset = buffer.readInt32LE(COLOR_OFFSET);
+  parsedBitmap.fileSize = buffer.readUInt32LE(FILE_SIZE_OFFSET);
+  parsedBitmap.pixelTableOffset = buffer.readUInt32LE(PIXEL_TABLE_OFFSET);
+  parsedBitmap.height = buffer.readUInt32LE(HEIGHT_OFFSET);
+  parsedBitmap.headerSize = buffer.readUInt32LE(HEADER_OFFSET);
+  parsedBitmap.colorOffset = buffer.readUInt32LE(COLOR_OFFSET);
+  parsedBitmap.bitPerPixelOffset = buffer.readUInt32LE(BIT_PER_PIXEL_OFFSET);
+  parsedBitmap.width = buffer.readUInt32LE(WIDTH_OFFSET);
 
   //TODO: find where colors start and how to access
   //TODO: find where pixels start
