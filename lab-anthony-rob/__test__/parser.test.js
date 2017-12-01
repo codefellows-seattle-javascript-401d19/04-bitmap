fe6e338c02ddb1efd0d25e63be7c5f990104b463
Robert Reed <robert.mc.reed@gmail.com>
2017-11-30 18:39:02 -0800

    first transform test passing

--- lab-anthony-rob/__test__/parser.test.js
+++ lab-anthony-rob/__test__/parser.test.js
@@ -4,14 +4,14 @@ const asset = require('./assets/testAsset.js');
 const parser = require('../lib/parser.js');
 
 describe('parser.js', () => {
-  const bufferData = {
-    buffer: asset.buffer,
-    type: asset.buffer.toString('utf-8', 0,2),
-    filesize: asset.buffer.readInt32LE(2),
-    pixelTableOffset: asset.buffer.readInt32LE(10),
-    height: asset.buffer.readInt32LE(22),
-  };
+  // const bufferData = {
+  //   buffer: asset.buffer,
+  //   type: asset.buffer.toString('utf-8', 0,2),
+  //   filesize: asset.buffer.readInt32LE(2),
+  //   pixelTableOffset: asset.buffer.readInt32LE(10),
+  //   height: asset.buffer.readInt32LE(22),
+  // };
   test('parser should return an object containing the metadata', () => {
-    expect(parser(asset.buffer)).toMatchObject(bufferData);
+    expect(parser(asset.buffer)).toMatchObject(asset.bmpData);
   });
 });
--- lab-anthony-rob/__test__/transform.test.js
+++ lab-anthony-rob/__test__/transform.test.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const asset = require('./assets/testAsset.js');
+
+const transform = require('../lib/transform.js');
+
+describe('transform.js', () => {
+  test('transform should return a buffer', () => {
+    expect(transform(asset.bmpData, 0)).toBeDefined();
+  });
+});
