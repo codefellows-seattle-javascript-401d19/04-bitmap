6ffee4d30bb14b0528e5a9c1cd172b37771a08ed
snobeard <fatmattmatt@gmail.com>
2017-11-30 18:20:51 -0800

    a-c-p ing

--- lab-matt/index.js
+++ lab-matt/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const bitmap = require('./lib/bitmap');
 let files = fs.readdirSync('./__test__/assets/');
 
-bitmap.bufferFile(files[0], (err) => console.log(err));
+bitmap.bufferFile(files[2], (err) => console.log(err));
 console.log( fs.readFileSync(`${__dirname}/__test__/assets/bitmap.bmp`) );
 
 let myCLI = function(input_file_path, output_file_path, transfrom_name) {
--- lab-matt/lib/bitmap.js
+++ lab-matt/lib/bitmap.js
@@ -43,30 +43,20 @@ bitmap.createBufferObject = (buffer) => {
   parsedBitmap.width = buffer.readInt32LE(offset.PIXEL_WIDTH);
   parsedBitmap.height = buffer.readInt32LE(offset.PIXEL_WIDTH);
   parsedBitmap.bitsPerPixel = buffer.readInt8(offset.BITS_PER_PIXEL);
-  // parsedBitmap.newBuffer = buffer.writeUInt8('0x4',10);
+  parsedBitmap.newBuffer = sizeTwoHundred(buffer);
   
   
-  fs.writeFile('output.bmp', parsedBitmap.buffer, err => console.log(err));
+  
+  fs.writeFile('output.bmp', parsedBitmap.newBuffer, err => console.log(err));
   fs.writeFile('output.json', JSON.stringify(parsedBitmap.buffer), err => console.log(err));
   
 
   return parsedBitmap;
 };
 
-
-// bitmap.parseBitmap = (buffer) => {
-//   let parsedBitmap = {};
-
-//   const HEIGHT_OFFSET = 22;
-//   const PIXEL_TABLE_OFFSET = 10;
-//   const FILE_SIZE_OFFSET = 2;
-
-//   parsedBitmap.buffer = buffer;
-//   // vinicio - we are taking the first 2 characters because of the docs
-//   //           https://en.wikipedia.org/wiki/BMP_file_format
-//   parsedBitmap.type = buffer.toString('utf-8',0,2);
-//   parsedBitmap.fileSize = buffer.readInt32LE(FILE_SIZE_OFFSET);
-//   parsedBitmap.pixelTableOffset = buffer.readInt32LE(PIXEL_TABLE_OFFSET);
-//   parsedBitmap.height = buffer.readInt32LE(HEIGHT_OFFSET);
-//   return parsedBitmap;
-// };
+let sizeTwoHundred = (buffer) => {
+  // return buffer.writeUInt32LE(100, 18);
+  return buffer;
+  
+  
+};
