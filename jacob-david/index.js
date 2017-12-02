d1fb17948b9cc1251a40dd00512253eb352e5ea4
Jacob M-G Evans <jacobmgevans@gmail.com>
2017-12-01 21:27:14 -0800

    working in transform

--- jacob-david/index.js
+++ jacob-david/index.js
@@ -1,19 +1,26 @@
 'use strict';
 
 
-const transformedBitmap = require('./lib/transform')
+const transformedBitmap = require('./lib/transform');
 const fs = require('fs');
 
 // Read file path needs to be dynamic TODO: likely just a parameter that has an argument passed through from the test.(PARAMETER YES "PATH")
 fs.readFile(`${__dirname}/asset/house.bmp`, (error,data) => {
-  if(error)
-  {
+  if(error) {
     console.error(error);
     return;
   }
 
+  // transformedBitmap.invert(data);
 
-  let transformedBitmapData = transformedBitmap.invertColors(data);
+  // transformedBitmap.greenRedSwitch(data);
   
   // console.log(transformedBitmapData);
+});
+
+fs.writeFile('NEWHOUSE.bmp', (data, err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+
+  return transformedBitmap.greyScale(data);
 });
--- jacob-david/lib/transform.js
+++ jacob-david/lib/transform.js
@@ -1,22 +1,38 @@
 'use strict';
 
 const transformedBitmap = module.exports = {};
+
 const bitmap = require('./bitmap');
 
 
 // TODO: pass in the callback next to the buffer
-transformedBitmap.invertColors = (buffer) => {
+transformedBitmap.greyScale = (buffer) => {
+  let parsedBitmapArray = bitmap.parseBitmap(buffer);
   
-
-
-  let parsedBitmap = bitmap.parseBitmap(buffer);
-  console.log('====================================');
-  console.log(...parsedBitmap);
-  
-  console.log('====================================');  
-
-  // transformedBitmap.color = buffer.fill(97);
-  
-  // return transformedBitmap;
-};
+  for (let i = 0; i < parsedBitmapArray.length; i+=4) {
     
+    let grey = Math.floor((parsedBitmapArray[i-3] + parsedBitmapArray[i-2] + parsedBitmapArray[i-1])/3);
+    
+    parsedBitmapArray[i-3] = grey
+    parsedBitmapArray[i-2] = grey
+    parsedBitmapArray[i-1] = grey
+    console.log('====================================');
+    console.log(...parsedBitmapArray);
+    console.log('====================================');
+  }  
+  return transformedBitmap.greyScale();
+};
+
+// transformedBitmap.inverted = (buffer) => {
+//   let parsedBitmapArray = bitmap.parseBitmap(buffer);
+  
+//   for (let i = 0; i < parsedBitmapArray.length; i+=4) {
+//     const element = parsedBitmapArray[i];
+//     let grey = (element[i-3] + element[i-2] + element[i-1])/3;
+//     element[i-3] = grey;
+//     element[i-2] = grey;
+//     element[i-1] = grey;
+//   }  
+//   // return transformedBitmap.greyScale;
+// };
+  
