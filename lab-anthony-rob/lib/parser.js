981252e248697f814afea50f45cb5a360ead37f0
Robert Reed <robert.mc.reed@gmail.com>
2017-12-02 12:39:48 -0800

    removed old comments from transofrm.js

--- lab-anthony-rob/lib/parser.js
+++ lab-anthony-rob/lib/parser.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// added constants so there are no 'magic' numbers
 const FILESIZE_OFFSET = 2;
 const PIXEL_ARRAY_LOCATION_OFFSET = 10;
 const WIDTH_OFFSET = 18;
@@ -17,6 +16,6 @@ module.exports = (buffer) => {
     height: buffer.readInt32LE(HEIGHT_OFFSET),
   };
   bufferData.colorPalleteBuffer = buffer.slice(COLOR_TABLE_OFFSET, bufferData.pixelArrayOffset);
-  // might need to subtract 2 from pixelArrayOffset
+
   return bufferData;
 };
