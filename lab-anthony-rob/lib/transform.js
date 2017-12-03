daa33c88df2e70cd81f32f845178de280e3917b1
Robert Reed <robert.mc.reed@gmail.com>
2017-12-02 18:27:59 -0800

    fixed small pallette typo in transforms

--- lab-anthony-rob/lib/transform.js
+++ lab-anthony-rob/lib/transform.js
@@ -3,15 +3,15 @@
 const transforms = {};
 
 transforms.grayscaleAvg = bmpData => {
-  let buff = bmpData.colorPalleteBuffer;
+  let buff = bmpData.colorPalletteBuffer;
   for(let i = 0; i < buff.length; i += 4) {
-    let average = (buff[i] + buff[i + 1] + buff[i + 2])/3;
+    let average = (buff[i] + buff[i + 1] + buff[i + 2]) / 3;
     buff[i] = buff[i + 1] = buff[i + 2] = average;
   }
 };
 
 transforms.invert = bmpData => {
-  let buff = bmpData.colorPalleteBuffer;
+  let buff = bmpData.colorPalletteBuffer;
   for(let i = 0; i < buff.length; i += 4) {
     buff[i] = 255 - buff.readInt8(i);
     buff[i + 1] = 255 - buff.readInt8(i + 1);
