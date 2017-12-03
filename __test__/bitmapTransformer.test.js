d09cbecf995a768dd42c199feebd483c4d9df9e1
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-03 11:06:20 -0800

    fixed data persistence by saving function outputs to variables

--- __test__/bitmapTransformer.test.js
+++ __test__/bitmapTransformer.test.js
@@ -6,7 +6,7 @@ const bmpPaths = fs.readdirSync(`${__dirname}/../asset`);
 
 describe(`bitmapTransformer.js`, () => {
   test(`greyscale image should be returned for greyscale transform function`, () => {
-    expect(bitmapTransformer.transform(bmpPaths[2], `transformedHouse.bmp`, `greyscale`)).not.toBeNull();
+    expect(bitmapTransformer.alterImage(bmpPaths[2], `transformedHouse.bmp`, `greyscale`)).not.toBeNull();
   })
   // test(`inverted color image should be returned for inverted color transform function`, () => {
   //   expect(bitmapTransformer.transform(bmpPaths[2], `transformedHouse.bmp`, `invert`)).not.toBeNull();
