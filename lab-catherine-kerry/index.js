f91b547d920835b1e130f77bf7b13cc6dfb97ff9
Catherine Looper <loopercat@gmail.com>
2017-12-03 15:42:24 -0800

    Set up CLI functionality

--- lab-catherine-kerry/index.js
+++ lab-catherine-kerry/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const bitmap = require('./lib/bitmap');
+const transform = require('./lib/transformMods');
 const fs = require('fs');
 const image = 'house.bmp';
-// const myArgs = process.argv.slice();
-const transform = require('./lib/transformMods');
+const infile = process.argv[2];
+const outfile = process.argv[3];
+const myArgs = process.argv.slice(4);
 
 
 fs.readFile(`${__dirname}/__test__/asset/${image}`, (error,data) => {
@@ -15,10 +17,23 @@ fs.readFile(`${__dirname}/__test__/asset/${image}`, (error,data) => {
   }
 
   let parsedBitmap = bitmap.parseBitmap(data);
-  transform.grayscale(parsedBitmap);
-  transform.invert(parsedBitmap);
-  transform.random(parsedBitmap);
 
+  switch (myArgs[0]) {
+  case 'grayscale':
+    transform.grayscale(parsedBitmap);
+    console.log(myArgs[1], 'a grayscale image has been created.');
+    break;
+  case 'invert':
+    transform.invert(parsedBitmap);
+    console.log(myArgs[1], 'an inverted image has been created.');
+    break;
+  case 'random':
+    transform.random(parsedBitmap);
+    console.log(myArgs[1], 'a random image has been created.');
+    break;
+  default:
+    console.log('Sorry, you did not list a valid transform method');
+  }
 
   fs.writeFile(`${__dirname}/__test__/asset/test${image}`, parsedBitmap.buffer, error => {
     if(error)
@@ -26,7 +41,7 @@ fs.readFile(`${__dirname}/__test__/asset/${image}`, (error,data) => {
       console.error(error);
       return;
     }
-    console.log('written');
+    console.log('Congratulations, your photo file has been written!');
   });
 });
 
