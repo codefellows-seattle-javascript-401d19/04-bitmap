2027dde8bce530cffaab7f4ca3db951ed7d42679
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2017-12-03 14:32:40 -0800

    Drafted all three transform functions

--- lab-catherine-kerry/index.js
+++ lab-catherine-kerry/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const bitmap = require('./lib/bitmap');
+const bitmap = require('./lib/transformMods');
 const fs = require('fs');
 const image = 'house.bmp';
 // const myArgs = process.argv.slice();
@@ -15,7 +15,7 @@ fs.readFile(`${__dirname}/__test__/asset/${image}`, (error,data) => {
 
   let parsedBitmap = bitmap.parseBitmap(data);
   console.log(parsedBitmap);
-  transformMods(parsedBitmap)
+
 
   fs.writeFile(`${__dirname}/__test__/asset/test${image}`, data, error => {
     if(error)
--- lab-catherine-kerry/lib/transformMods.js
+++ lab-catherine-kerry/lib/transformMods.js
@@ -1,23 +1,77 @@
 'use strict';
 
-const transformMods = {};
+const bitmap = module.exports = {};
 
-grayscale.parseBitmap = (buffer) => {
+bitmap.parseBitmap = (buffer) => {
   let parsedBitmap = {};
+
+  const HEIGHT_OFFSET = 22;
+  const PIXEL_TABLE_OFFSET = 10;
+  const FILE_SIZE_OFFSET = 2;
+
+  parsedBitmap.buffer = buffer;
+  // vinicio - we are taking the first 2 characters because of the docs
+  //           https://en.wikipedia.org/wiki/BMP_file_format
+  parsedBitmap.type = buffer.toString('utf-8', 0, 2);
+  parsedBitmap.fileSize = buffer.readInt32LE(FILE_SIZE_OFFSET);
+  parsedBitmap.pixelTableOffset = buffer.readInt32LE(PIXEL_TABLE_OFFSET);
+  parsedBitmap.height = buffer.readInt32LE(HEIGHT_OFFSET);
+  parsedBitmap.colorTable = buffer.slice(54, 1077);
 
   parsedBitmap.colorTable.forEach((value, position, array) => {
 
+    // if (position % 4 === 1) {
+   
+    //   parsedBitmap.colorTable.writeUInt8(array[position - 1], position);
+ 
+    // }
+    // if (position % 4 === 2) {
+   
+    //   parsedBitmap.colorTable.writeUInt8(array[position - 2], position);
+
+    // }
+    // if (position % 4 === 3) {
+
+    //   parsedBitmap.colorTable.writeUInt8(array[position - 3], position);
+ 
+    // }
+
+    // if (position % 4 === 1) {
+    
+    //   parsedBitmap.colorTable.writeUInt8(255 - array[position], position);
+      
+    // }
+    // if (position % 4 === 2) {
+
+    //   parsedBitmap.colorTable.writeUInt8(255 - array[position], position);
+    
+    // }
+    // if (position % 4 === 3) {
+  
+    //   parsedBitmap.colorTable.writeUInt8(255 - array[position], position);
+
+    // }
+
+
     if (position % 4 === 1) {
-      parsedBitmap.colorTable.writeUInt8((array[position - 1]), position);
+
+      parsedBitmap.colorTable.writeUInt8(Math.abs((Math.random() * 255) - array[position]), position);
+
     }
     if (position % 4 === 2) {
-      parsedBitmap.colorTable.writeUInt8((array[position - 2]), position);
-      console.log(parseInt(array[position - 2]));
+
+      parsedBitmap.colorTable.writeUInt8(Math.abs((Math.random() * 255) - array[position]), position);
+
     }
     if (position % 4 === 3) {
-      parsedBitmap.colorTable.writeUInt8((array[position - 3]), position);
-    }
 
+      parsedBitmap.colorTable.writeUInt8(Math.abs((Math.random() * 255) - array[position]), position);
+
+    }
   });
+  console.log(parsedBitmap.colorTable = buffer.slice(57, 1077));
+  console.log(parsedBitmap);
   return parsedBitmap;
+
 };
+
