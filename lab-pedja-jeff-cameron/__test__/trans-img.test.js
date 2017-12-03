ff5134702027598efccb8627a8eef47a1cc98ca3
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-02 23:13:39 -0800

    restructuring trans-img tests

--- lab-pedja-jeff-cameron/__test__/trans-img.test.js
+++ lab-pedja-jeff-cameron/__test__/trans-img.test.js
@@ -1,87 +1,69 @@
 'use strict';
 
 const { transFile } = require('../lib/trans-img');
-const { getFile } = require('../lib/read-img');
 
 describe('trans-img.js', () => {
-  test('return acidified image where all G values are equal to 0', done => {
-    const inputFilePath = `${__dirname}/assets/house.bmp`;
-    const expected = 0;
+  test('return acidified image where G value is equal to 0', done => {
     const transformName = 'acid';
+    const constructedBitmap = {};
+    constructedBitmap.colorTable = [0, 128, 128, 128];
+    const expected = 0;
     let result = null;
-    getFile(inputFilePath, (error, data) => {
-      if(error) {
-        console.error(error);
-        return;
-      }
-      let constructedBitmap = data;
-      transFile(transformName, constructedBitmap, (error, data) => {
-        result = data.colorTable[2];
-        expect(error).toBeNull();
-        expect(result).toEqual(expected);
-        done();
-      });
-    });
+
+    const callback = (error, data) => {
+      result = data;
+      expect(error).toBeNull();
+      expect(result.colorTable[2]).toEqual(expected);
+      done();
+    };
+
+    transFile(transformName, constructedBitmap, callback);
   });
 
   test('return greyscale image in which all RGB values in single color are same', done => {
     const transformName = 'greyscale';
-    const inputFilePath = `${__dirname}/assets/house.bmp`;
-    let expectedRed = null;
-    let expectedGreen = null;
-    let expectedBlue = null;
-    getFile(inputFilePath, (error, data) => {
-      if(error) {
-        console.error(error);
-        return;
-      }
-      let constructedBitmap = data;
-      transFile(transformName, constructedBitmap, (error, data) => {
-        expectedRed = data.colorTable[1];
-        expectedGreen = data.colorTable[2];
-        expectedBlue = data.colorTable[3];
-        expect(error).toBeNull();
-        expect(expectedRed).toEqual(expectedBlue);
-        expect(expectedBlue).toEqual(expectedGreen);
-        done();
-      });
-    });
+    const constructedBitmap = {};
+    constructedBitmap.colorTable = [0, 50, 150, 250];
+    let expected = [0, 1, 1, 1];
+    let result = null;
+
+    const callback = (error, data) => {
+      result = data;
+      expect(error).toBeNull();
+      expect(result.colorTable).toEqual(expected);
+      done();
+    };
+
+    transFile(transformName, constructedBitmap, callback);
   });
 
   test('return inverted image in which RGB value are modified', done => {
     const transformName = 'invert';
-    const inputFilePath = `${__dirname}/assets/house.bmp`;
-    getFile(inputFilePath, (error, data) => {
-      if(error) {
-        console.error(error);
-        return;
-      }
-      let constructedBitmap = data;
+    const constructedBitmap = {};
+    constructedBitmap.colorTable = [0, 0, 55, 155];
+    let result = null;
+    let expected = [0, 255, 200, 100];
 
-      // Pedja - this callback function needs verification for inverting image, only testing that error is null.
-      const callback = error => {
-        expect(error).toBeNull();
-        done();
-      };
+    const callback = (error, data) => {
+      result = data;
+      expect(error).toBeNull();
+      expect(result.colorTable).toEqual(expected);
+      done();
+    };
 
-      transFile(transformName, constructedBitmap, callback);
-    });
+    transFile(transformName, constructedBitmap, callback);
   });
 
   test('throw an error if invalid image transformation', done => {
     const transformName = 'invalid';
-    const inputFilePath = `${__dirname}/assets/house.bmp`;
-    getFile(inputFilePath, (error, data) => {
-      if(error) {
-        console.error(error);
-        return;
-      }
-      let constructedBitmap = data;
-      transFile(transformName, constructedBitmap, error => {
-        expect(error).toString('Please use one of the following values: \n greyscale \n invert \n magenta');
-        // console.log(error);
-        done();
-      });
-    });
+    const constructedBitmap = {};
+    constructedBitmap.colorTable = [0, 0, 55, 155];
+    let expected = 'Please use one of the following values: \n greyscale \n invert \n acid';
+
+    const callback = (error) => {
+      expect(error).toString(expected);
+      done();
+    };
+    transFile(transformName, constructedBitmap, callback);
   });
 });
