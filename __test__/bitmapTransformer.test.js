54a07351a3dbe5c00ebbf28df68ff9c8bc563eaa
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-03 20:34:54 -0800

    finish writing tests

--- __test__/bitmapTransformer.test.js
+++ __test__/bitmapTransformer.test.js
@@ -3,16 +3,27 @@
 const fs = require(`fs`);
 const bitmapTransformer = require(`../lib/bitmapTransformer`);
 const indexJS = require(`../index.js`);
-const bmpPaths = fs.readdirSync(`${__dirname}/../asset`);
 
-// describe(`bitmapTransformer.js`, () => {
-//   test(`greyscale image should be returned for greyscale transform function`, () => {
-//     expect(indexJS.transformImage(bmpPaths[2], `newImage.bmp`, [`invert`, `greyscale`])).not.toBeNull();
-//   })
-//   // test(`inverted color image should be returned for inverted color transform function`, () => {
-//   //   expect(bitmapTransformer.transform(bmpPaths[2], `transformedHouse.bmp`, `invert`)).not.toBeNull();
-//   // })
-//   // test(`randomized color image should be returned for randomized color transform function`, () => {
-//   //   expect(bitmapTransformer.transform(bmpPaths[2], `transformedHouse.bmp`, `randomize`)).not.toBeNull();
-//   // })
-// });
+describe(`bitmapTransformer.js`, () => {
+  test(`indexJS.transformImage should return an edited image based on the transform operation provided if no errors are present`, () => {
+    expect(() => {
+      indexJS.transformImage(`house.bmp`, `output.bmp`, 'invert');
+    }).not.toThrow();
+    expect(() => {
+      indexJS.transformImage(`house.bmp`, `output.bmp`, 'greyscale');
+    }).not.toThrow();
+    expect(() => {
+      indexJS.transformImage(`house.bmp`, `output.bmp`, 'randomize');
+    }).not.toThrow();
+  });
+  test(`indexJS.transformImage should return an error if the transform type provided is not one of the three options`, () => {
+    expect(() => {
+      indexJS.transformImage(`house.bmp`, `output.bmp`, [blah]);
+    }).toThrow();
+  });
+  test(`indexJS.transformImage should return an error if fewer than three parameters are provided`, () => {
+    expect(() => {
+      indexJS.transformImage(`house.bmp`, `output.bmp`);
+    }).toThrow();
+  });
+});
