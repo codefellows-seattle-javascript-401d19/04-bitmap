1f862b71ae3e486eacff5070c3eeb54ca0dc4a4c
Dalton Carr <daltonlcarr@yahoo.com>
2017-12-04 14:25:42 -0800

    index js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const bitmap = require('./lib/bitmap');
+const fs = require('fs');
+
+fs.readFile(`${__dirname}/__test__/asset/house.bmp`, (error,data) => {
+  if(error)
+  {
+    console.error(error);
+    return;
+  }
+
+  let parsedBitmap = bitmap.parseBitmap(data);
+  console.log(parsedBitmap);
+
+  fs.writeFile(`${__dirname}/__test__/asset/houseTest.bmp`, data, error => {
+    if(error)
+    {
+      console.error(error);
+      return;
+    }
+  });
+});
+
