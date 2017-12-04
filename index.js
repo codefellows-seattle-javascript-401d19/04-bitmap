93c9c3a7e2c4f2f96a9d44bdb1879873c77d815b
Dalton Carr <daltonlcarr@yahoo.com>
2017-12-04 14:31:34 -0800

    created black and white image

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const bitmap = require('./lib/bitmap');
 const fs = require('fs');
 
-fs.readFile(`${__dirname}/__test__/asset/house.bmp`, (error,data) => {
+fs.readFile(`${__dirname}/asset/house.bmp`, (error,data) => {
   if(error)
   {
     console.error(error);
@@ -13,7 +13,7 @@ fs.readFile(`${__dirname}/__test__/asset/house.bmp`, (error,data) => {
   let parsedBitmap = bitmap.parseBitmap(data);
   console.log(parsedBitmap);
 
-  fs.writeFile(`${__dirname}/__test__/asset/houseTest.bmp`, data, error => {
+  fs.writeFile(`${__dirname}/asset/houseTest.bmp`, data, error => {
     if(error)
     {
       console.error(error);
