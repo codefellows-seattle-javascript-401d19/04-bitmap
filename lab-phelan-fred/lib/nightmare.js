e87afa6701740af2287ac241d46e50404ce566a5
dataluxe <phelan.carey@gmail.com>
2017-12-03 22:51:38 -0800

    Scaffolded main function. Big parts left to do - Build constructor, build transform function, end-to-end proof-of-life. Then polishing, tests, done.

---
+++ lab-phelan-fred/lib/nightmare.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//require fs
+//reset module.exports;
+
+//const greyscale = (data) => {
+
+//With whatever kind of <split>, <map> <for loop>, or whatever other means which is easiest, cleanest, and most robust, modify 1) the Color Palette byte ranges of the incoming <File> object.
+
+//Research if there's anything else you need to modify. If so, modify those byte ranges here.
+
+//Write all the byte ranges back into one buffer, and RETURN that buffer.
+//}
+
+//module.export this function.
