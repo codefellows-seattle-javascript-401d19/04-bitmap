129a4ea16f565479e1eac851506cc9aeea0dba48
FREDRIC THOMAS RAPP <fred.rapp@gmail.com>
2017-12-05 03:03:36 -0800

    added greyscale transform codebase

--- lab-phelan-fred/lib/greyscale.js
+++ lab-phelan-fred/lib/greyscale.js
@@ -3,8 +3,15 @@
 const greyscale = data => {
 
 //With whatever kind of <split>, <map> <for loop>, or whatever other means which is easiest, cleanest, and most robust, modify the Color Palette byte ranges of the incoming <File> object to achieve the desired effect.
-
-//Write all the byte ranges back into one buffer, and RETURN that buffer.
+  transform.greyscale = (colorTable) => {
+    let greyscale;
+    for (var i = 0; i < colorTable.length; i+= 4) {
+      greyscale = colorTable[i] + colorTable[i + i] + colorTable[i + 2] / 3;
+      colorTable[i] = colorTable[i + i] = colorTable[i + 2] = greyscale;
+    }
+    return colorTable;
+  };
+  //Write all the byte ranges back into one buffer, and RETURN that buffer.
 
   const buffer = 0;
   buffer;
