e4c9ede7f7af2850f18ee1d5fc6178b7f6c18816
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-06 14:54:58 -0800

    still trying to fix bug testing bitmap function

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const indexJS = module.exports = {};
 let args = process.argv.slice(2);
 
 indexJS.transformImage = (inputPath, outputPath, transforms) => {
-  if(!inputPath || !outputPath || transforms.length < 1){
+  if(!inputPath || !outputPath || transforms.length < 1){ //this + calling the function at the end is what's breaking my tests
     throw new Error(`Please provide an input path, an output path, and at least one transformation type`);
   }
 
-  bitmapTransformer.readFile(inputPath, (data) => {
+  bitmapTransformer.readFile(inputPath, (data) => { //for this function to run correctly transforms must have at least on argument
     let parsedBitmap = bitmapTransformer.getColorPalette(data);
 
     for(let i = 0; i < transforms.length; i++){
@@ -40,4 +40,4 @@ indexJS.transformImage = (inputPath, outputPath, transforms) => {
   }
 };
 
-indexJS.transformImage(args[0], args[1], args.slice(2));
+indexJS.transformImage(args[0], args[1], args.slice(2));  //this runs even for my tests, and breaks them because I get an error that I'm not passing in any arguments
