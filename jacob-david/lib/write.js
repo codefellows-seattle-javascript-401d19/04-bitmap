3b9dd71ab4d90d633a0646c6f29fdb770203ce7b
David Lindahl <davidalindahl@gmail.com>
2017-12-10 12:40:08 -0800

    added readwrite function to the write js file

--- jacob-david/lib/write.js
+++ jacob-david/lib/write.js
@@ -2,8 +2,29 @@
 
 const useIndex = require(`../index`);
 const fs = require('fs');
+const bitmap = require('./bitmap');
+const transform = require('./transform');
 
-useIndex.read.fs.writeFile('NEWHOUSE.bmp',data, (err) => {
-  if (err) throw err;
-  console.log('The file has been saved!');
-});
+const readWrite = module.exports = {};
+
+// useIndex.read.fs.writeFile('NEWHOUSE.bmp',data, (err) => {
+//   if (err) throw err;
+//   console.log('The file has been saved!');
+// });
+
+readWrite.readBMP = (bmpPath, transform, callback) => {
+  if(!bmpPath)
+    callback(new Error('must provide a valid path'));
+
+  fs.readFile(`${__dirname}/../assets/${bmpPath}`, (error,bitmapFile) => {
+    if(error){
+      callback(error);      
+      console.error(error);
+    }
+    console.log('read source bmp');
+
+    if(callback){
+      transform[transform](bitmap.parseBitmap(bitmapFile),callback);
+    }
+  });
+};
