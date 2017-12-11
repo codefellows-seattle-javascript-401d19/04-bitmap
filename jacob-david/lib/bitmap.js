1e141c7568d15ee8835a12f7ad7064b97ffb46ee
David Lindahl <davidalindahl@gmail.com>
2017-12-10 17:16:36 -0800

    updates so the parsed bitmap is operated on

--- jacob-david/lib/bitmap.js
+++ jacob-david/lib/bitmap.js
@@ -8,10 +8,10 @@ bitmapModule.parseBitmap = (buffer) => {
   const HEIGHT_OFFSET = 22;
   const PIXEL_TABLE_OFFSET = 10;
   const FILE_SIZE_OFFSET = 2;
-  const HEADER_OFFSET = 22;
-  const COLOR_OFFSET = 18;
-  const BIT_PER_PIXEL_OFFSET = 28;
-  const WIDTH_OFFSET = 18;
+  // const HEADER_OFFSET = 22;
+  // const COLOR_OFFSET = 18;
+  // const BIT_PER_PIXEL_OFFSET = 28;
+  // const WIDTH_OFFSET = 18;
   const HEADER_AND_DIB_OFFSET = 54;
 
   parsedBitmap.buffer = buffer;
@@ -23,7 +23,7 @@ bitmapModule.parseBitmap = (buffer) => {
   parsedBitmap.pixelTableOffset = buffer.readUInt32LE(PIXEL_TABLE_OFFSET);
   // parsedBitmap.colorOffset = buffer.readUInt32LE(COLOR_OFFSET);
   // parsedBitmap.bitPerPixelOffset = buffer.readUInt32LE(BIT_PER_PIXEL_OFFSET);
-  parsedBitmap.colorPalleteSection = buffer.slice(HEADER_AND_DIB_OFFSET, this.pixelTableOffset); 
+  parsedBitmap.colorPalleteSection = buffer.slice(HEADER_AND_DIB_OFFSET, parsedBitmap.pixelTableOffset); 
   
   return parsedBitmap; 
 };
