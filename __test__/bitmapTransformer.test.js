02a491c1c6aaa72cd187bb9f250cdd051d32f89e
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-12 17:59:02 -0800

    successfully separate image transform function from function call; tests pass but don't produce a new bitmap image

--- __test__/bitmapTransformer.test.js
+++ __test__/bitmapTransformer.test.js
@@ -1,34 +1,34 @@
 'use strict';
 
-const imageTransformer = require(`../imageTransformer.js`);
+const imageTransformer = require(`../lib/imageTransformer.js`);
 
 let args;
 
 describe(`bitmapTransformer.js`, () => {
-  test(`indexJS.transformImage should return an edited image based on the transform operation provided if no errors are present`, () => {
+  test(`imageTransformer.transformImage should return an edited image based on the transform operation provided if no errors are present`, () => {
     expect(() => {
       args = [`house.bmp`,`invert.bmp`,`invert`];
-      indexJS.transformImage(...args);
+      imageTransformer.transformImage(...args);
     }).not.toThrow();
     expect(() => {
       args = [`house.bmp`,`greyscale.bmp`,`greyscale`];
-      indexJS.transformImage(...args);
+      imageTransformer.transformImage(...args);
     }).not.toThrow();
     expect(() => {
       args = [`house.bmp`,`randomize.bmp`,`randomize`];
-      indexJS.transformImage(...args);
+      imageTransformer.transformImage(...args);
     }).not.toThrow();
   });
-  test(`indexJS.transformImage should return an error if the transform type provided is not one of the three options`, () => {
+  test(`imageTransformer.transformImage should return an error if the transform type provided is not one of the three options`, () => {
     expect(() => {
       args = [`house.bmp`,`randomize.bmp`,[blah]];
-      indexJS.transformImage(...args);
+      imageTransformer.transformImage(...args);
     }).toThrow();
   });
-  test(`indexJS.transformImage should return an error if fewer than three parameters are provided`, () => {
+  test(`imageTransformer.transformImage should return an error if fewer than three parameters are provided`, () => {
     expect(() => {
       args = [`house.bmp`,`randomize.bmp`];
-      indexJS.transformImage(...args);
+      imageTransformer.transformImage(...args);
     }).toThrow();
   });
 });
--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const indexJS = module.exports = {};
 
 let args = process.argv.slice(2);
 
-indexJS.transformImage(args[0], args[1], args.slice(2));
+imageTransformer.transformImage(args[0], args[1], args.slice(2));
